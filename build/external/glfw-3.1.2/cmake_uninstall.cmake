6086fd25add250a607af9d61a33dd373da920409
Léa <leaserrano1119@gmail.com>
2022-10-31 22:11:32 +0100

    Essai avancée

--- build/external/glfw-3.1.2/cmake_uninstall.cmake
+++ build/external/glfw-3.1.2/cmake_uninstall.cmake
@@ -1,22 +1,22 @@
 
-if (NOT EXISTS "/home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt")
-  message(FATAL_ERROR "Cannot find install manifest: \"/home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt\"")
+if (NOT EXISTS "/home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt")
+  message(FATAL_ERROR "Cannot find install manifest: \"/home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt\"")
 endif()
 
-file(READ "/home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt" files)
+file(READ "/home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/external/glfw-3.1.2/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
   message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
   if (EXISTS "$ENV{DESTDIR}${file}")
-    exec_program("/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    exec_program("/snap/cmake/1156/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
       MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
     endif()
   elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
-    EXEC_PROGRAM("/usr/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    EXEC_PROGRAM("/snap/cmake/1156/bin/cmake" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
--- build/launch-gmap.sh
+++ build/launch-gmap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 bindir=$(pwd)
-cd /home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/src/
+cd /home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/src/
 export 
 
 if test "x$1" = "x--debugger"; then
@@ -8,10 +8,10 @@ if test "x$1" = "x--debugger"; then
 	if test "xYES" = "xYES"; then
 		echo "r  " > $bindir/gdbscript
 		echo "bt" >> $bindir/gdbscript
-		/usr/bin/gdb -batch -command=$bindir/gdbscript --return-child-result /home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap 
+		/usr/bin/gdb -batch -command=$bindir/gdbscript --return-child-result /home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap 
 	else
-		"/home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap"  
+		"/home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap"  
 	fi
 else
-	"/home/e20190003227/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap"  
+	"/home/lea/M1-IMAGINE/Modélisation3D/M1-IMAGINE-Modelisation3D-TP6/build/gmap"  
 fi
